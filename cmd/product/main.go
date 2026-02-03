845285ccb9362ee1218ac06e2ee23c332a7e67fd
王红岩 lWoHvYe <lwohvye@outlook.com>
2026-02-04 00:57:24 +0800

    etcd run

--- cmd/product/main.go
+++ cmd/product/main.go
@@ -32,7 +32,7 @@ var (
 )
 
 func init() {
-	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
+	flag.StringVar(&flagconf, "conf", "/kratos/configs/product", "config path, eg: -conf config.yaml")
 }
 
 // main.go 创建 kratos 应用生命周期管理
